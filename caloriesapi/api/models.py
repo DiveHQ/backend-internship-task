026b1a862ad6c5fefc040b8b42810147aac173f5
ralphvw <114740626+ralphvw@users.noreply.github.com>
2023-06-09 14:44:07 +0000

    Calories model

--- caloriesapi/api/models.py
+++ caloriesapi/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Calories(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    text = models.CharField(max_length=250)
+    calories = models.PositiveIntegerField(blank=True, null=True)
+    is_below_expected = models.BooleanField(blank=True, null=True)
+    
+    def get_calories(self):
+        if not self.calories:
+            pass
