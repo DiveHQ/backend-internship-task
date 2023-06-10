3a8d7678034265e0e69273b45e6d215991b5341c
ralphvw <114740626+ralphvw@users.noreply.github.com>
2023-06-10 11:04:05 +0000

    Added max_calories to user

--- caloriesapi/api/models.py
+++ caloriesapi/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 
 
 class Calories(models.Model):
@@ -9,7 +9,14 @@ class Calories(models.Model):
     text = models.CharField(max_length=250)
     calories = models.PositiveIntegerField(blank=True, null=True)
     is_below_expected = models.BooleanField(blank=True, null=True)
-    
+
     def get_calories(self):
         if not self.calories:
             pass
+    
+    def set_below_expected(self):
+        self.is_below_expected = self.user.max_calories < self.calories
+
+
+class User(AbstractUser):
+    max_calories = models.PositiveIntegerField()
