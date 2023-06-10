8cdfad89e750ffda888db71a3832f848e8ddeed0
Emmanuel Tiboah <etiboah@gmail.com>
2023-06-10 22:24:21 +0000

    add and configure environment variables for the project

---
+++ core/configvars.py
@@ -0,0 +1,16 @@
+
+
+from pydantic import BaseSettings
+
+
+class EnvConfig(BaseSettings):
+    SECRET: str = "NOT THE REAL SECRET"
+    NUTRIXION_APP_ID: str = "NOT REAL APP ID"
+    NUTRIXION_APP_KEY: str = "NOT REAL APP KEY"
+    URL: str = "NOT REAL URL"
+
+    class Config:
+        env_file = ".env"
+
+
+env_config = EnvConfig()
