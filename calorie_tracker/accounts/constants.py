890fbac69538d6f585e619470a9cdb7f98433e28
snnbotchway <solomonbotchway7@gmail.com>
2023-06-11 22:45:11 +0000

    Improve user list API filtering

--- calorie_tracker/accounts/constants.py
+++ calorie_tracker/accounts/constants.py
@@ -3,3 +3,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 USER_MANAGER_GROUP = "User Managers"
+
+COMMON_FILTERS = {
+    "contains_exact": ["contains", "startswith", "endswith", "exact", "iexact"],
+    "gt_lt": ["gt", "lt", "gte", "lte"],
+}
--- calorie_tracker/accounts/filters.py
+++ calorie_tracker/accounts/filters.py
@@ -1,20 +1,24 @@
+from django_filters import CharFilter
 from django_filters.rest_framework import FilterSet
 
+from .constants import COMMON_FILTERS as f
 from .constants import User
 
 
 class UserFilter(FilterSet):
     """Filter class for the User model."""
 
+    username = CharFilter()
+
     class Meta:
         model = User
         fields = {
-            "username": ["contains"],
-            "first_name": ["contains"],
-            "last_name": ["contains"],
-            "email": ["contains"],
+            "username": f["contains_exact"],
+            "first_name": f["contains_exact"],
+            "last_name": f["contains_exact"],
+            "email": f["contains_exact"],
             "is_staff": ["exact"],
             "is_active": ["exact"],
             "is_superuser": ["exact"],
-            "date_joined": ["gt", "lt"],
+            "date_joined": f["gt_lt"],
         }
