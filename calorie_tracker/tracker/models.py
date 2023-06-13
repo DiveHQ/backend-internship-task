bc1bff8d6aefc2ed9965ca12dfa703dd61bd871f
snnbotchway <solomonbotchway7@gmail.com>
2023-06-13 10:18:40 +0000

    Cleanup models

--- calorie_tracker/tracker/models.py
+++ calorie_tracker/tracker/models.py
@@ -5,12 +5,34 @@ from django.db import models
 class Entry(models.Model):
     """Entry model."""
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="entries")
-    text = models.TextField()
-    date = models.DateField(auto_now_add=True)
-    time = models.TimeField(auto_now_add=True)
-    calories = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    below_daily_threshold = models.BooleanField(default=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="entries",
+        help_text="The user associated with this entry",
+    )
+    text = models.TextField(
+        help_text=(
+            "The description of the meal(s) taken. "
+            "Please prefix each meal with the quantity of any measurement."
+        )
+    )
+    date = models.DateField(auto_now_add=True, help_text="The date of the entry")
+    time = models.TimeField(auto_now_add=True, help_text="The time of the entry")
+    calories = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True,
+        help_text="The number of calories in the meal(s)",
+    )
+    below_daily_threshold = models.BooleanField(
+        default=True,
+        help_text=(
+            "Indicates if this entry causes the assigned user's total calories for the day "
+            "to be equal to or exceed their expected daily calories"
+        ),
+    )
 
     class Meta:
         verbose_name_plural = "Entries"
