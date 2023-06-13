aa7305e2e1f3a75451a94891df051c8b408fa3d8
snnbotchway <solomonbotchway7@gmail.com>
2023-06-13 13:07:40 +0000

    Build user settings API

--- calorie_tracker/accounts/urls.py
+++ calorie_tracker/accounts/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import UserViewSet
+from .views import UserSettingViewSet, UserViewSet
 
 app_name = "accounts"
 
 router = DefaultRouter()
 router.register("users", UserViewSet)
+router.register("user-settings", UserSettingViewSet)
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
