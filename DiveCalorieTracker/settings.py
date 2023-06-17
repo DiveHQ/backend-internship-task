0dec926282a057d6e8e152e3d9d244e872150bdf
vibhav.10 <vibhav.1507@gmail.com>
2023-06-17 17:04:43 +0530

    update main urls and add drf_yasg

--- DiveCalorieTracker/settings.py
+++ DiveCalorieTracker/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_yasg',
     'users',
 ]
 
--- DiveCalorieTracker/urls.py
+++ DiveCalorieTracker/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for DiveCalorieTracker project.
+"""DiveCalorieTracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DiveCalorieTracker API",
+        default_version='v1',
+        description="Welcome to DiveCalorieTracker API Documentation",
+        #terms_of_service="",
+        contact=openapi.Contact(email="vibhav.1507@gmail.com"),
+        #license=openapi.License(name=""),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(r'^doc(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
 ]
