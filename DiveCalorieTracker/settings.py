b3b54a5755ff8e5f2d94e3a1e36abc585ae9287a
vibhav.10 <vibhav.1507@gmail.com>
2023-06-19 17:15:28 +0530

    remove unnecessary code

--- DiveCalorieTracker/settings.py
+++ DiveCalorieTracker/settings.py
@@ -147,12 +147,5 @@ REST_FRAMEWORK = {
 # Custom user model
 AUTH_USER_MODEL = 'users.User'
 
-#SMTP SETTINGS
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS =  True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 API_KEY = os.getenv('API_KEY')
