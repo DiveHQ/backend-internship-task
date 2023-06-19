afe59c3640367bc18410f9367efb090359bcb0ca
ombhojane <ombhojane05@gmail.com>
2023-06-19 21:04:43 +0530

    Added authentication

--- api.py
+++ api.py
@@ -36,3 +36,28 @@ class CalorieEntry(db.Model):
     text = db.Column(db.String(100))
     calories = db.Column(db.Integer)
     is_below_expected = db.Column(db.Boolean, default=False)
+
+# Authentication Decorator
+
+def authenticate(role):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            auth_header = request.headers.get('Authorization')
+            if not auth_header:
+                return jsonify({'message': 'Authorization required'}), 401
+            
+            try:
+                token = auth_header.split(' ')[1]
+                payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+                user_id = payload['user_id']
+                user = User.query.get(user_id)
+                if not user or user.role != role:
+                    return jsonify({'message': 'Unauthorized'}), 401
+                return func(*args, **kwargs)
+            except jwt.ExpiredSignatureError:
+                return jsonify({'message': 'Token has expired'}), 401
+            except (jwt.InvalidTokenError, IndexError):
+                return jsonify({'message': 'Invalid token'}), 401
+        return wrapper
+    return decorator
